4e0477ffe44a0481c28675227f3c28f64232d571
Jordan Buckmaster <buckmast@purdue.edu>
2018-09-16 22:32:09 -0400

    Set up main routing and skeleton components

--- client/src/components/course/CoursePanel.js
+++ client/src/components/course/CoursePanel.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react';
-import logo from '../../img/encourse-logo-large.png';
-import "../../css/Login.css";
+import React, { Component } from 'react'
+
+import '../../css/CoursePanel.css'
 
 class CoursePanel extends Component {
-    render() {
-        return (
-            <div className="Login">
-                <header className="Login-header">
-                    <img src={logo} className="EnCourse-logo" alt="logo" />
-                </header>
-                <div className="credentials-container">
-                    <form>
-                        <p>Username</p>
-                        <input type="text" />
-                        <p>Password</p>
-                        <input type="text" />
-                        <br />
-                        <input type="submit" value="Enter Credentials" />
-                    </form>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="CoursePanel">
+        
+      </div>
+    )
+  }
 }
 
-export default CoursePanel;
+export default CoursePanel
