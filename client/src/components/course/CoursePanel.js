5bf7cfa8748b6a0942554c41479fb72c125a0d84
Killian-LeClainche <kleclain@purdue.edu>
2018-09-16 23:40:03 -0400

    Implemented Basic Navbar and included image files.

--- client/src/components/course/CoursePanel.js
+++ client/src/components/course/CoursePanel.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 
 import '../../css/CoursePanel.css'
+import Card from '../Card'
+import CourseSettings from 'CourseSettings'
+import StudentPreview from 'StudentPreview'
 
 class CoursePanel extends Component {
-  render() {
-    return (
-      <div className="CoursePanel">
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="CoursePanel">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        )
+    }
 }
 
 export default CoursePanel
