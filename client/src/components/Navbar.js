a0d32b4c1d15b6c3226a86a5518d51f27e7c18b6
Killian-LeClainche <kleclain@purdue.edu>
2018-09-16 23:40:03 -0400

    Implemented Basic Navbar and included image files.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 
 import '../css/Navbar.css'
-import logo from '../img/encourse-logo-large.png'
+import settingsIcon from '../img/settings.svg'
+import logoutIcon from '../img/logout.svg'
 import { history } from '../redux/store'
 
 class Navbar extends Component {
-  render() {
-    return (
-      <div className="Navbar">
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="Navbar">
+                <div className="EnCourse-Title">
+                    <div>En</div>
+                    <div>Course</div>
+                </div>
+                <div className="Options">
+                    <div className="Settings">
+                        <img src={settingsIcon} />
+                    </div>
+                    <div className="Logout">
+                        <img src={logoutIcon} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Navbar
