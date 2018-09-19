e5dc62efae77523ec4d3ba010f13a22b5ef810d4
Jordan Buckmaster <buckmast@purdue.edu>
2018-09-19 00:56:03 -0400

    Fix conflict in Card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import '../css/Card.css'
-=======
-import logo from '../img/encourse-logo-large.png';
->>>>>>> master
 
 class Card extends Component {
     render() {
