dab2744c94e6b60917c8cab7e7bd9782af101e9f
jarett-lee <jarett.lee.pi@gmail.com>
2018-09-18 22:24:05 -0400

    Create class progress histogram chart

--- client/src/components/ClassProgressHistogram.js
+++ client/src/components/ClassProgressHistogram.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const data = [
-    {name: 'Student A', progress: .50},
-    {name: 'Student B', progress: .30},
-    {name: 'Student C', progress: .50},
-    {name: 'Student D', progress: .50},
-    {name: 'Student E', progress: .00},
-    {name: 'Student F', progress: .60},
-    {name: 'Student G', progress: .50},
+const students = [
+    {name: 'Student A', progress: 50},
+    {name: 'Student B', progress: 30},
+    {name: 'Student C', progress: 17},
+    {name: 'Student D', progress: 39},
+    {name: 'Student E', progress: 0},
+    {name: 'Student F', progress: 10},
+    {name: 'Student G', progress: 0},
+    {name: 'Student H', progress: 1},
+    {name: 'Student I', progress: 15},
+    {name: 'Student J', progress: 5},
+    {name: 'Student K', progress: 20},
+    {name: 'Student L', progress: 10},
+    {name: 'Student M', progress: 100},
+    {name: 'Student N', progress: 40},
+    {name: 'Student O', progress: 0},
+    {name: 'Student P', progress: 0},
 ];
+
+const data = [];
+
+const binCount = 6;
+const binSize = 100 / binCount;
+
+for (let i = 0; i < binCount; i++) {
+    data[i] = {
+        progressBin: `${parseInt(i * binSize)}-${parseInt((i + 1) * binSize)}%`,
+        count: 0,
+    }
+}
+
+for (let student of students) {
+    const progressBin = Math.min(parseInt(student.progress / binSize), binCount-1);
+    data[progressBin].count += 1;
+}
 
 class ClassProgressHistogram extends Component {
     render() {
@@ -17,10 +43,10 @@ class ClassProgressHistogram extends Component {
             <ComposedChart layout="vertical" width={600} height={400} data={data}>
                 <CartesianGrid/>
                 <XAxis type="number"/>
-                <YAxis dataKey="name" type="category"/>
+                <YAxis dataKey="progressBin" type="category"/>
                 <Tooltip/>
                 <Legend/>
-                <Bar dataKey="progress" fill="#8884d8"/>
+                <Bar dataKey="count" fill="#8884d8"/>
             </ComposedChart>
         );
     }
