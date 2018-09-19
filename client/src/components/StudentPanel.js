359975090f9795e255021da3556036be66021564
Jordan Buckmaster <buckmast@purdue.edu>
2018-09-19 02:54:34 -0400

    Set up basic student progress graph with fake data

--- client/src/components/StudentPanel.js
+++ client/src/components/StudentPanel.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react'
 import { LineChart, CartesianGrid, XAxis, YAxis, 
-  Tooltip, Legend, Line } from 'recharts'
+  Tooltip, Legend, Line, Label } from 'recharts'
+import { timeFormat } from 'd3-time-format'
+import { scaleTime } from 'd3-scale'
+import { timeDay } from 'd3-time'
 
 import Card from './Card'
 
 
 class StudentPanel extends Component {
+
+  formatTicks = (tick) => {
+    console.log(tick)
+    return timeFormat('%-m/%-d')(tick)
+  }
+
   render() {
     const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-   ]
+      {name: '9/10', progress: 0},
+      {name: '9/11', progress: 20},
+      {name: '9/12', progress: 20},
+      {name: '9/13', progress: 30},
+      {name: '9/14', progress: 50},
+      {name: '9/15', progress: 60},
+      {name: '9/16', progress: 100},
+    ]
     return (
       <div className="StudentPanel">
       
         <Card component={
-            <LineChart width={730} height={250} data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <LineChart width={730} height={500} data={data}
+            margin={{ top: 10, right: 30, left: 20, bottom: 20 }}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis dataKey="name">
+              <Label value="Date" position="bottom" />
+            </XAxis>
+            <YAxis>
+              <Label angle={-90} position='insideLeft' style={{ textAnchor: 'middle' }}>
+                % Completion
+              </Label>
+            </YAxis>
             <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            <Legend verticalAlign="top"/>
+            <Line type="monotone" dataKey="progress" stroke="#8884d8" />
           </LineChart>}
         />
       </div>
