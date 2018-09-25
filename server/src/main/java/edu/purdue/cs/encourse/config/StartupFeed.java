9452854459ee691c2091c83572944f09c829921f
Shawn <shawnkmontgomery@gmail.com>
2018-09-25 14:35:17 -0400

    Modify feed to include proper types for account retrieval

--- server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
+++ server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
@@ -20,13 +20,13 @@ public class StartupFeed implements ApplicationListener<ApplicationReadyEvent> {
     
     private void feedDatabase() {
         if (adminService.findAllUsers().isEmpty()) {
-            adminService.addAccount("0", "grr", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Gustavo", "Rodriguez-Rivera", "2", "A", "grr@purdue.edu");
-            adminService.addAccount("1", "buckmast", "$2a$04$9c76evM3G9DGPy0SoSvA7uH567Raz6Tuv5vTeV/BxL.3gNSel1POK", "Jordan", "Buckmaster", "3", "M", "buckmast@purdue.edu");
-            adminService.addAccount("2", "kleclain", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Killian", "LeClainche", "3", "A", "kleclain@purdue.edu");
-            adminService.addAccount("3", "lee2363", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Jarett", "Lee", "3", "B", "lee2363@purdue.edu");
-            adminService.addAccount("4", "montgo38", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Shawn", "Montgomery", "3", "K", "montgo38@purdue.edu");
-            adminService.addAccount("5", "reed226", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "William", "Reed", "3", "J", "reed226@purdue.edu");
-            adminService.addAccount("6", "sullil96", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Ryan", "Sullivan", "3", "P", "sulli196@purdue.edu");
+            adminService.addAccount("0", "grr", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Gustavo", "Rodriguez-Rivera", "Professor", "A", "grr@purdue.edu");
+            adminService.addAccount("1", "buckmast", "$2a$04$9c76evM3G9DGPy0SoSvA7uH567Raz6Tuv5vTeV/BxL.3gNSel1POK", "Jordan", "Buckmaster", "Admin", "M", "buckmast@purdue.edu");
+            adminService.addAccount("2", "kleclain", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Killian", "LeClainche", "Admin", "A", "kleclain@purdue.edu");
+            adminService.addAccount("3", "lee2363", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Jarett", "Lee", "Admin", "B", "lee2363@purdue.edu");
+            adminService.addAccount("4", "montgo38", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Shawn", "Montgomery", "Admin", "K", "montgo38@purdue.edu");
+            adminService.addAccount("5", "reed226", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "William", "Reed", "Admin", "J", "reed226@purdue.edu");
+            adminService.addAccount("6", "sullil96", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", "Ryan", "Sullivan", "Admin", "P", "sulli196@purdue.edu");
 
             adminService.addUser("grr", "$2a$04$KDYkLNaDhiKvMqJhRQ58iumiMAd8Rxf4az3COnKsPKNlHcK7PMjs6", false, false, false, true);
             adminService.addUser("buckmast", "$2a$04$9c76evM3G9DGPy0SoSvA7uH567Raz6Tuv5vTeV/BxL.3gNSel1POK", false, false, false, true);
