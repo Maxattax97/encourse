57f9a0020b2059ed80a211365df8c0e80d1ca0dd
jarett-lee <jarett.lee.pi@gmail.com>
2018-09-25 17:31:59 -0400

    Add chart titile to commit frequency chart

--- client/src/components/charts/CommitFrequencyHistogram.js
+++ client/src/components/charts/CommitFrequencyHistogram.js
@@ -26,6 +26,7 @@ class CommitHistoryHistogram extends Component {
     render() {
         return (
             <div classname="chart-container">
+                <h3 className="chart-title">Commit Frequency</h3>
                 <BarChart width={600} height={300} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                     <CartesianGrid/>
                     <XAxis dataKey="dateStr"/>
