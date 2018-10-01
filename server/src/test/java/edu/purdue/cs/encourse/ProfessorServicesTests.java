98ddd2c418328bd62d8f8de468a98b98d006c1a1
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-09-30 23:16:50 -0400

    Adjust content of a test script in test script test

--- server/src/test/java/edu/purdue/cs/encourse/ProfessorServicesTests.java
+++ server/src/test/java/edu/purdue/cs/encourse/ProfessorServicesTests.java
@@ -122,7 +122,7 @@ public class ProfessorServicesTests {
         assertNotNull(professorService.countAllCommitsByDay(Project.createProjectID("cs250", "Fall2018", "MyMalloc")));
         assertNotNull(professorService.countStudentCommitsByDay(Project.createProjectID("cs250", "Fall2018", "MyMalloc"), "dwyork"));
         assertNotNull(professorService.listStudentCommitsByTime(Project.createProjectID("cs250", "Fall2018", "MyMalloc"), "rravind"));
-        assertEquals(0, professorService.uploadTestScript(Project.createProjectID("cs250", "Fall2018", "MyMalloc"), "ls_test.sh", "ls"));
+        assertEquals(0, professorService.uploadTestScript(Project.createProjectID("cs250", "Fall2018", "MyMalloc"), "ls_test.sh", "ls .."));
         assertEquals(0, professorService.uploadTestScript(Project.createProjectID("cs250", "Fall2018", "MyMalloc"), "echo_test.sh", "echo \"\""));
         assertEquals(0, professorService.runTestall(Project.createProjectID("cs250", "Fall2018", "MyMalloc")));
         List<StudentProject> projects = studentProjectRepository.findByIdStudentID("1");
