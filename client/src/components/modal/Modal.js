99e1c5cb6747192a4fe79fa214d89976afd6e7a5
Killian-LeClainche <kleclain@purdue.edu>
2018-10-17 14:58:55 -0400

    Tweak Course Modal

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 
-import exitIcon from '../../resources/x.svg'
 import { Card } from '../Helpers'
+import { x } from '../../helpers/icons'
 
 class Modal extends Component {
 
     render() {
         return (
-            <div className={'modal'} style={this.props.show ? {} : {display: 'none'}} onClick={ this.props.onExit }>
+            <div className='modal' style={this.props.show ? {} : {display: 'none'}} onClick={ this.props.onExit }>
                 <div className={this.props.left ? 'modal-left' : this.props.right ? 'modal-right' : 'modal-center'} onClick={(e) => e.stopPropagation()}>
-                    <Card component={<div className={'modal-container'}>
-                        {this.props.component}
-                        <div className="exit-nav" onClick={ this.props.onClose || this.props.onExit }>
-                            <img src={exitIcon} alt="" />
-                        </div>
-                    </div>} />
+                    <Card component={
+                        [
+                            <div className={'modal-container'} key={1}>
+                                {this.props.component}
+                            </div>,
+                            <div className="action svg-icon exit-nav" onClick={ this.props.onClose || this.props.onExit } key={2}>
+                                <img className='svg-icon' src={ x.icon } alt={ x.alt_text } />
+                            </div>
+                        ]
+
+                    } />
                 </div>
             </div>
         )
