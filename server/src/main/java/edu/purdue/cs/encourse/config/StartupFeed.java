fa34fc65d574138c5160625019107480f91939da
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-10-16 22:34:42 -0400

    Change limit for number of students in database to 100

--- server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
+++ server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
@@ -74,7 +74,7 @@ public class StartupFeed implements ApplicationListener<ApplicationReadyEvent> {
                 BufferedReader fileReader = new BufferedReader(new FileReader("/sourcecontrol/cs252/Fall2018/students.txt"));
                 String student = null;
                 int count = 1;
-                while((student = fileReader.readLine()) != null && count <= 10) {
+                while((student = fileReader.readLine()) != null && count <= 100) {
                     if(student.equals("grr")) {
                         continue;
                     }
