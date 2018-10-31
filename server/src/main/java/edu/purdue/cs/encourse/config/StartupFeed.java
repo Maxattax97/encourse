a3b337ca9ef6a6da3836000d4530caf1049fa289
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-10-31 02:04:22 -0400

    Enable automatic testing for malloc

--- server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
+++ server/src/main/java/edu/purdue/cs/encourse/config/StartupFeed.java
@@ -186,11 +186,9 @@ public class StartupFeed implements ApplicationListener<ApplicationReadyEvent> {
         else {
             List<Project> projects = projectRepository.findBySemesterAndCourseID("Fall2018", "cs252");
             for(Project p : projects) {
-                if(p.getProjectName().equals("Shell")) {
-                    p.setTestRate(4);
-                    p.setTestCount(0);
-                    projectRepository.save(p);
-                }
+                p.setTestRate(4);
+                p.setTestCount(0);
+                projectRepository.save(p);
             }
         }
     }
