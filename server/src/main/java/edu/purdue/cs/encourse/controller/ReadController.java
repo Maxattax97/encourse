662611959429fca7eed4113e5223dbd604c2d444
Shawn <shawnkmontgomery@gmail.com>
2018-10-31 11:10:34 -0400

    Add sorting and pagination to student data endpoints

--- server/src/main/java/edu/purdue/cs/encourse/controller/ReadController.java
+++ server/src/main/java/edu/purdue/cs/encourse/controller/ReadController.java
@@ -357,14 +357,14 @@ public class ReadController {
     }
 
 
-
+    //TODO: Update to consume list of students
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/commitList", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<?> getStudentCommitByTime(@RequestParam(name = "projectID") String projectID,
                                                                   @RequestParam(name = "userName") List<String> userNames,
                                                                   @RequestParam(name = "page", defaultValue = "1", required = false) int page,
                                                                   @RequestParam(name = "size", defaultValue = "10", required = false) int size,
-                                                                  @RequestParam(name = "sortBy", defaultValue = "id", required = false) String sortBy) throws Exception {
+                                                                  @RequestParam(name = "sortBy", defaultValue = "date", required = false) String sortBy) {
         List<String> errors = new ArrayList<>();
         List<String> correct = new ArrayList<>();
         JSONReturnable returnJson = null;
@@ -442,7 +442,6 @@ public class ReadController {
         JSONArray sortedAndPagedJsonArray = new JSONArray();
         page = (page > jsonValues.size() / size + 1) ? jsonValues.size() / size + 1 : page;
         for (int i = (page - 1) * size; i < jsonValues.size(); i++) {
-            System.out.println(i + ": " + jsonValues.get(i));
             if (i >= page * size) {
                 break;
             }
@@ -463,15 +462,19 @@ public class ReadController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    //TODO: Update to consume list of students
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/commitCount", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<?> getCommitCount(@RequestParam(name = "projectID") String projectID,
-                                                          @RequestParam(name = "userName") List<String> userNames) {
+                                                          @RequestParam(name = "userName") List<String> userNames,
+                                                          @RequestParam(name = "page", defaultValue = "1", required = false) int page,
+                                                          @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+                                                          @RequestParam(name = "sortBy", defaultValue = "date", required = false) String sortBy) {
         List<String> errors = new ArrayList<>();
         List<String> correct = new ArrayList<>();
+        JSONReturnable returnJson = null;
         for (String userName: userNames) {
             if (hasPermissionOverAccount(userName)) {
-                JSONReturnable returnJson = null;
                 Iterator iter = getUserAuthorities().iterator();
                 while (iter.hasNext()) {
                     String auth = ((Authority) iter.next()).getAuthority();
@@ -494,10 +497,65 @@ public class ReadController {
                 errors.add("\"" + getUserFromAuth().getUsername() + " does not have access over " + userName + "\"");
             }
         }
-        if (errors.isEmpty()) {
-            return new ResponseEntity<>(correct.get(0), HttpStatus.OK);
+
+        JSONArray json = (JSONArray) returnJson.getJsonObject().get("data");
+        List<JSONObject> jsonValues = new ArrayList<>();
+        for (int i = 0; i < json.size(); i++) {
+            JSONObject obj = (JSONObject) json.get(i);
+            jsonValues.add(obj);
         }
-        return new ResponseEntity<>("{\"errors\": " + errors + ", \"correct\": " + correct + "}", HttpStatus.BAD_REQUEST);
+
+        Comparator<JSONObject> compare;
+        switch(sortBy) {
+            case "date":
+                compare = (JSONObject a, JSONObject b) -> {
+                    String valA = (String) a.get(sortBy);
+                    String valB = (String) b.get(sortBy);
+                    Date parsedA;
+                    Date parsedB;
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+                        parsedA = format.parse(valA);
+                        parsedB = format.parse(valB);
+                    }
+                    catch(ParseException pe) {
+                        throw new IllegalArgumentException(pe);
+                    }
+                    return parsedA.compareTo(parsedB);
+                };
+                break;
+            case "count":
+            default:
+                compare = (JSONObject a, JSONObject b) -> {
+                    long valA = (long) a.get(sortBy);
+                    long valB = (long) b.get(sortBy);
+                    return Long.compare(valA, valB);
+                };
+                break;
+        }
+        jsonValues.sort(compare);
+
+        JSONArray sortedAndPagedJsonArray = new JSONArray();
+        page = (page > jsonValues.size() / size + 1) ? jsonValues.size() / size + 1 : page;
+        for (int i = (page - 1) * size; i < jsonValues.size(); i++) {
+            if (i >= page * size) {
+                break;
+            }
+            sortedAndPagedJsonArray.add(jsonValues.get(i));
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("content", sortedAndPagedJsonArray);
+        response.put("totalPages", jsonValues.size() / size + 1);
+        response.put("page", page);
+        response.put("totalSize", jsonValues.size());
+        response.put("size", size);
+        response.put("elements", sortedAndPagedJsonArray.size());
+        response.put("sortedBy", sortBy);
+        response.put("last", (page >= jsonValues.size() / size));
+        response.put("first", (page == 1));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -540,12 +598,15 @@ public class ReadController {
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/diffs", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<?> getDiffs(@RequestParam(name = "projectID") String projectID,
-                                                    @RequestParam(name = "userName") List<String> userNames) {
+                                                    @RequestParam(name = "userName") List<String> userNames,
+                                                    @RequestParam(name = "page", defaultValue = "1", required = false) int page,
+                                                    @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+                                                    @RequestParam(name = "sortBy", defaultValue = "date", required = false) String sortBy) {
         List<String> errors = new ArrayList<>();
         List<String> correct = new ArrayList<>();
+        JSONReturnable returnJson = null;
         for (String userName: userNames) {
             if (hasPermissionOverAccount(userName)) {
-                JSONReturnable returnJson = null;
                 Iterator iter = getUserAuthorities().iterator();
                 while (iter.hasNext()) {
                     String auth = ((Authority) iter.next()).getAuthority();
@@ -568,17 +629,76 @@ public class ReadController {
                 errors.add("\"" + getUserFromAuth().getUsername() + " does not have access over " + userName + "\"");
             }
         }
-        if (errors.isEmpty()) {
-            return new ResponseEntity<>(correct.get(0), HttpStatus.OK);
+
+        JSONArray json = (JSONArray) returnJson.getJsonObject().get("data");
+        List<JSONObject> jsonValues = new ArrayList<>();
+        for (int i = 0; i < json.size(); i++) {
+            JSONObject obj = (JSONObject) json.get(i);
+            jsonValues.add(obj);
         }
-        return new ResponseEntity<>("{\"errors\": " + errors + ", \"correct\": " + correct + "}", HttpStatus.BAD_REQUEST);
+
+        Comparator<JSONObject> compare;
+        switch(sortBy) {
+            case "date":
+                compare = (JSONObject a, JSONObject b) -> {
+                    String valA = (String) a.get(sortBy);
+                    String valB = (String) b.get(sortBy);
+                    Date parsedA;
+                    Date parsedB;
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+                        parsedA = format.parse(valA);
+                        parsedB = format.parse(valB);
+                    }
+                    catch(ParseException pe) {
+                        throw new IllegalArgumentException(pe);
+                    }
+                    return parsedA.compareTo(parsedB);
+                };
+                break;
+            case "additions":
+            case "deletions":
+            default:
+                compare = (JSONObject a, JSONObject b) -> {
+                    long valA = (long) a.get(sortBy);
+                    long valB = (long) b.get(sortBy);
+                    return Long.compare(valA, valB);
+                };
+                break;
+        }
+        jsonValues.sort(compare);
+
+        JSONArray sortedAndPagedJsonArray = new JSONArray();
+        page = (page > jsonValues.size() / size + 1) ? jsonValues.size() / size + 1 : page;
+        for (int i = (page - 1) * size; i < jsonValues.size(); i++) {
+            if (i >= page * size) {
+                break;
+            }
+            sortedAndPagedJsonArray.add(jsonValues.get(i));
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("content", sortedAndPagedJsonArray);
+        response.put("totalPages", jsonValues.size() / size + 1);
+        response.put("page", page);
+        response.put("totalSize", jsonValues.size());
+        response.put("size", size);
+        response.put("elements", sortedAndPagedJsonArray.size());
+        response.put("sortedBy", sortBy);
+        response.put("last", (page >= jsonValues.size() / size));
+        response.put("first", (page == 1));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/velocity", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<?> getVelocity(@RequestParam(name = "projectID") String projectID,
-                                                       @RequestParam(name = "userName", required = false) List<String> userNames) {
+                                                       @RequestParam(name = "userName", required = false) List<String> userNames,
+                                                       @RequestParam(name = "page", defaultValue = "1", required = false) int page,
+                                                       @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+                                                       @RequestParam(name = "sortBy", defaultValue = "date", required = false) String sortBy) {
         List<String> errors = new ArrayList<>();
         List<String> correct = new ArrayList<>();
 
@@ -612,17 +732,77 @@ public class ReadController {
                 errors.add("\"" + getUserFromAuth().getUsername() + " does not have access over " + userName + "\"");
             }
         }
-        if (errors.isEmpty()) {
-            return new ResponseEntity<>(correct.get(0), HttpStatus.OK);
+
+        JSONArray json = (JSONArray) returnJson.getJsonObject().get("data");
+        List<JSONObject> jsonValues = new ArrayList<>();
+        for (int i = 0; i < json.size(); i++) {
+            JSONObject obj = (JSONObject) json.get(i);
+            jsonValues.add(obj);
         }
-        return new ResponseEntity<>("{\"errors\": " + errors + ", \"correct\": " + correct + "}", HttpStatus.BAD_REQUEST);
+
+        Comparator<JSONObject> compare;
+        switch(sortBy) {
+            case "date":
+                compare = (JSONObject a, JSONObject b) -> {
+                    String valA = (String) a.get(sortBy);
+                    String valB = (String) b.get(sortBy);
+                    Date parsedA;
+                    Date parsedB;
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+                        parsedA = format.parse(valA);
+                        parsedB = format.parse(valB);
+                    }
+                    catch(ParseException pe) {
+                        throw new IllegalArgumentException(pe);
+                    }
+                    return parsedA.compareTo(parsedB);
+                };
+                break;
+            case "commit_count":
+            case "progress":
+            case "time_spent":
+            default:
+                compare = (JSONObject a, JSONObject b) -> {
+                    long valA = (long) a.get(sortBy);
+                    long valB = (long) b.get(sortBy);
+                    return Long.compare(valA, valB);
+                };
+                break;
+        }
+        jsonValues.sort(compare);
+
+        JSONArray sortedAndPagedJsonArray = new JSONArray();
+        page = (page > jsonValues.size() / size + 1) ? jsonValues.size() / size + 1 : page;
+        for (int i = (page - 1) * size; i < jsonValues.size(); i++) {
+            if (i >= page * size) {
+                break;
+            }
+            sortedAndPagedJsonArray.add(jsonValues.get(i));
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("content", sortedAndPagedJsonArray);
+        response.put("totalPages", jsonValues.size() / size + 1);
+        response.put("page", page);
+        response.put("totalSize", jsonValues.size());
+        response.put("size", size);
+        response.put("elements", sortedAndPagedJsonArray.size());
+        response.put("sortedBy", sortBy);
+        response.put("last", (page >= jsonValues.size() / size));
+        response.put("first", (page == 1));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/progress", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<?> getProgress(@RequestParam(name = "projectID") String projectID,
-                                                       @RequestParam(name = "userName", required = false) List<String> userNames) {
+                                                       @RequestParam(name = "userName", required = false) List<String> userNames,
+                                                       @RequestParam(name = "page", defaultValue = "1", required = false) int page,
+                                                       @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+                                                       @RequestParam(name = "sortBy", defaultValue = "date", required = false) String sortBy) {
         List<String> errors = new ArrayList<>();
         List<String> correct = new ArrayList<>();
 
@@ -671,10 +851,64 @@ public class ReadController {
                 errors.add("\"" + getUserFromAuth().getUsername() + " does not have access over " + userName + "\"");
             }
         }
-        if (errors.isEmpty()) {
-            return new ResponseEntity<>(correct.get(0), HttpStatus.OK);
+        JSONArray json = (JSONArray) returnJson.getJsonObject().get("data");
+        List<JSONObject> jsonValues = new ArrayList<>();
+        for (int i = 0; i < json.size(); i++) {
+            JSONObject obj = (JSONObject) json.get(i);
+            jsonValues.add(obj);
         }
-        return new ResponseEntity<>("{\"errors\": " + errors + ", \"correct\": " + correct + "}", HttpStatus.BAD_REQUEST);
+
+        Comparator<JSONObject> compare;
+        switch(sortBy) {
+            case "date":
+                compare = (JSONObject a, JSONObject b) -> {
+                    String valA = (String) a.get(sortBy);
+                    String valB = (String) b.get(sortBy);
+                    Date parsedA;
+                    Date parsedB;
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+                        parsedA = format.parse(valA);
+                        parsedB = format.parse(valB);
+                    }
+                    catch(ParseException pe) {
+                        throw new IllegalArgumentException(pe);
+                    }
+                    return parsedA.compareTo(parsedB);
+                };
+                break;
+            case "progress":
+            default:
+                compare = (JSONObject a, JSONObject b) -> {
+                    long valA = (long) a.get(sortBy);
+                    long valB = (long) b.get(sortBy);
+                    return Long.compare(valA, valB);
+                };
+                break;
+        }
+        jsonValues.sort(compare);
+
+        JSONArray sortedAndPagedJsonArray = new JSONArray();
+        page = (page > jsonValues.size() / size + 1) ? jsonValues.size() / size + 1 : page;
+        for (int i = (page - 1) * size; i < jsonValues.size(); i++) {
+            if (i >= page * size) {
+                break;
+            }
+            sortedAndPagedJsonArray.add(jsonValues.get(i));
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("content", sortedAndPagedJsonArray);
+        response.put("totalPages", jsonValues.size() / size + 1);
+        response.put("page", page);
+        response.put("totalSize", jsonValues.size());
+        response.put("size", size);
+        response.put("elements", sortedAndPagedJsonArray.size());
+        response.put("sortedBy", sortBy);
+        response.put("last", (page >= jsonValues.size() / size));
+        response.put("first", (page == 1));
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
