00cdcf38ba2420051d46945a2b9adf1f28e9b09f
Killian-LeClainche <kleclain@purdue.edu>
2018-11-01 21:51:05 -0400

    Add new debug log

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -20,7 +20,6 @@ import StudentDishonestyPanel from './panel/StudentDishonestyPanel'
 class Main extends Component {
 
     createPanelRef = (node) => {
-        console.log('SET PANEL: ', node)
         if(node) {
             this.panel = node
 
@@ -29,7 +28,7 @@ class Main extends Component {
     }
 
     scrollPanel = (e) => {
-        console.log('SCROLL: ', e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight, this.currentChild)
+        console.log('SCROLL: ', e.currentTarget.scrollHeight, e.currentTarget.scrollTop, e.currentTarget.clientHeight)
         if(e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight && this.currentChild && this.currentChild.scrolledToBottom) {
             this.currentChild.scrolledToBottom()
         }
@@ -41,7 +40,6 @@ class Main extends Component {
     }
 
     setChild = (node) => {
-        console.log('SET CURRENT CHILD: ', node)
         if(node)
             this.currentChild = node.getWrappedInstance()
     }
