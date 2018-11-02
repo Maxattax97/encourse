df0f24c3a097021f9f77d042c3afbcc4632bdf7e
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-11-02 12:59:39 -0400

    Change class statistics to be based on all previous test results

--- server/src/main/java/edu/purdue/cs/encourse/service/impl/ProfessorServiceImpl.java
+++ server/src/main/java/edu/purdue/cs/encourse/service/impl/ProfessorServiceImpl.java
@@ -712,10 +712,25 @@ public class ProfessorServiceImpl implements ProfessorService {
             return new JSONReturnable(-2, null);
         }
         List<StudentProject> projects = studentProjectRepository.findByIdProjectIdentifier(projectID);
-        String visibleTestFile = "src/main/temp/" + Long.toString(Math.round(Math.random() * Long.MAX_VALUE)) + "_visibleTests.txt";
-        String hiddenTestFile = "src/main/temp/" + Long.toString(Math.round(Math.random() * Long.MAX_VALUE)) + "_hiddenTests.txt";
+        String visibleTestFile = "src/main/temp/" + Long.toString(Math.round(Math.random() * Long.MAX_VALUE)) + "_visibleTestsDates.txt";
+        String hiddenTestFile = "src/main/temp/" + Long.toString(Math.round(Math.random() * Long.MAX_VALUE)) + "_hiddenTestsDates.txt";
         try {
-            createTestFiles(visibleTestFile, hiddenTestFile, projects);
+            BufferedWriter visibleWriter = new BufferedWriter(new FileWriter(visibleTestFile));
+            BufferedWriter hiddenWriter = new BufferedWriter(new FileWriter(hiddenTestFile));
+            for(StudentProject project : projects) {
+                Student student = studentRepository.findByUserID(project.getStudentID());
+                List<StudentProjectDate> projectDates = studentProjectDateRepository.findByIdProjectIdentifierAndIdStudentID(projectID, student.getUserID());
+                visibleWriter.write("Start " + student.getUserName() + "\n");
+                hiddenWriter.write("Start " + student.getUserName() + "\n");
+                for (StudentProjectDate d : projectDates) {
+                    visibleWriter.write(d.getDate() + " " + d.getDateVisibleGrade() + "\n");
+                    hiddenWriter.write(d.getDate() + " " + d.getDateHiddenGrade() + "\n");
+                }
+                visibleWriter.write("End " + student.getUserName() + "\n");
+                hiddenWriter.write("End " + student.getUserName() + "\n");
+            }
+            visibleWriter.close();
+            hiddenWriter.close();
         } catch (IOException e) {
             return new JSONReturnable(-3, null);
         }
