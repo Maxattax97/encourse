e7002b23ed38be1ee804717a72bd08ffa2a4cd9e
jarett-lee <jarett.lee.pi@gmail.com>
2018-11-02 13:06:51 -0400

    Remove incomplete line

--- client/src/components/panel/student/chart/StudentProgressPerDay.js
+++ client/src/components/panel/student/chart/StudentProgressPerDay.js
@@ -109,7 +109,7 @@ class StudentProgressPerDay extends Component {
                     <ResponsiveContainer width="100%" height="100%">
                         <LineChart className="chart" width={730} height={500} data={this.state.formattedData}
                             margin={{ top: 20, right: 35, left: 20, bottom: 20 }}>
-                            <text className="chart-title" x="50%" y="15px" textAnchor="middle" dominantBaseline="middle">Student Progress Over Time (Incomplete)</text>
+                            <text className="chart-title" x="50%" y="15px" textAnchor="middle" dominantBaseline="middle">Student Progress Over Time</text>
                             <CartesianGrid strokeDasharray="3 3" />
                             <XAxis dataKey="date" type="number" domain={['dataMin', 'dataMax']} tickFormatter={this.dateFormatter}>
                                 <Label value="Date" position="bottom" />
