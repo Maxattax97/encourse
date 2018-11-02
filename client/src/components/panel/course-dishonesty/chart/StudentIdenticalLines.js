0c577616c13db460caa41e0e3215baa39b9e02a6
jarett-lee <jarett.lee.pi@gmail.com>
2018-11-01 23:43:16 -0400

    Add identical lines chart

--- client/src/components/panel/course-dishonesty/chart/StudentIdenticalLines.js
+++ client/src/components/panel/course-dishonesty/chart/StudentIdenticalLines.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react' 
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Label, ResponsiveContainer } from 'recharts'
+import { ScatterChart, Scatter, Dot, XAxis, YAxis, CartesianGrid, Tooltip, Label, ResponsiveContainer } from 'recharts'
 import { connect } from 'react-redux'
 
 import { getClassProgress } from '../../../../redux/actions/index'
@@ -18,41 +18,53 @@ const defaultData = [
         'user1index': 1,
         'user2index': 2,
         'simularity': 10,
+        'simularity-bin': 10,
+        'height': 2
     },
     {
         'user1': 'user1',
         'user2': 'user3',
         'user1index': 1,
         'user2index': 3,
+        'simularity-bin': 10,
         'simularity': 11,
+        'height': 3
     },
     {
         'user1': 'user1',
         'user2': 'user4',
         'user1index': 1,
         'user2index': 4,
+        'simularity-bin': 10,
         'simularity': 9,
+        'height': 1
     },
     {
         'user1': 'user2',
         'user2': 'user3',
         'user1index': 2,
         'user2index': 3,
+        'simularity-bin': 30,
         'simularity': 30,
+        'height': 1
     },
     {
         'user1': 'user2',
         'user2': 'user4',
         'user1index': 2,
         'user2index': 4,
+        'simularity-bin': 10,
         'simularity': 13,
+        'height': 5
     },
     {
         'user1': 'user3',
         'user2': 'user4',
         'user1index': 3,
         'user2index': 4,
+        'simularity-bin': 10,
         'simularity': 12,
+        'height': 4
     },
 ];
 
@@ -91,10 +103,30 @@ class StudentIdenticalLines extends Component {
     }
 
     render() {
+        console.log(this.state.formattedData);
         return (
             this.props.isLoading !== undefined && !this.props.isLoading
                 ? <div className="chart-container">
-                    <p>Chart not finished</p>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <ScatterChart
+                            data={this.state.formattedData}
+                            margin={{top: 5, right: 30, left: 30, bottom: 35}}
+                            barCategoryGap={0}
+                        >
+                            <XAxis dataKey="simularity-bin" type="number">
+                                <Label offset={-10} position="insideBottom">
+                                    Student
+                                </Label>
+                            </XAxis>
+                            <YAxis dataKey="height" type="number">
+                                <Label angle={-90} position='insideLeft' style={{ textAnchor: 'middle' }}>
+                                    Student
+                                </Label>
+                            </YAxis>
+                            <Tooltip/>
+                            <Scatter dataKey="count" fill="#8884d8"/>
+                        </ScatterChart>
+                    </ResponsiveContainer>
                 </div>
                 :
                 <div className='chart-container loading'>
