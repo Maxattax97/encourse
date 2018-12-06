5f76df74aa99fe85fdb1a00dc1f99de23e35ce75
jarett-lee <jarett.lee.pi@gmail.com>
2018-12-06 17:23:44 -0500

    Lint student charts

--- client/src/components/panel/student/chart/BrushSlider.js
+++ client/src/components/panel/student/chart/BrushSlider.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
-import { Brush, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Label, ResponsiveContainer } from 'recharts'
-import {getCurrentStudent, getStudentProgress} from "../../../../redux/state-peekers/student"
-import {getCurrentProject} from "../../../../redux/state-peekers/projects"
+import React from 'react'
+import { Brush, LineChart, Line, ResponsiveContainer } from 'recharts'
+import {getStudentProgress} from '../../../../redux/state-peekers/student'
 import { connect } from 'react-redux'
-import {Chart} from "../../../Helpers"
+import {Chart} from '../../../Helpers'
 import moment from 'moment'
 
 const dateFormatter = (dateUnix) => {
-	return moment(dateUnix).format('M-D')
+    return moment(dateUnix).format('M-D')
 }
 
 const BrushSlider = (props) => {
-    const {chart} = props;
+    const {chart} = props
     return (
-		<div style={{"height": "90px"}}>
-			<Chart chart={chart}>
-				<ResponsiveContainer width="100%" height="100%">
-					<LineChart data={chart.data} syncId="date" margin={{top: 0, right: 60, left: 60, bottom: 0}}>
-						<Line style={{"display": "none"}} dataKey="date" />
-						<Brush dataKey="date" height={60} stroke="#8884d8" tickFormatter={dateFormatter} x={60} y={20}/>
-				    </LineChart>
-				</ResponsiveContainer>
-			</Chart>
-		</div>
-	)
-};
+        <div style={{'height': '90px'}}>
+            <Chart chart={chart}>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart data={chart.data} syncId="date" margin={{top: 0, right: 60, left: 60, bottom: 0}}>
+                        <Line style={{'display': 'none'}} dataKey="date" />
+                        <Brush dataKey="date" height={60} stroke="#8884d8" tickFormatter={dateFormatter} x={60} y={20}/>
+                    </LineChart>
+                </ResponsiveContainer>
+            </Chart>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => {
-	return {
-		chart: getStudentProgress(state)
-	}
+    return {
+        chart: getStudentProgress(state)
+    }
 }
 
 export default connect(mapStateToProps)(BrushSlider)
