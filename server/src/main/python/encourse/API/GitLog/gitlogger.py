4711b86722938c9e997337397840f7853af6cb77
RyanNavillus <ryanpnavillus@gmail.com>
2018-12-06 11:49:09 -0500

    Add new commitsByDay method

--- server/src/main/python/encourse/API/GitLog/gitlogger.py
+++ server/src/main/python/encourse/API/GitLog/gitlogger.py
@@ -152,6 +152,29 @@ class GitLog:
         return additions, deletions
 
     def commitsByDay(self):
+        """Generates a list of commit objects with aggregated date from every commit from that day"""
+        current_date = None
+        daily_commits = {}
+        commits = []
+        for commit in self.commits:
+            if current_date == None:
+                # This is the first commit
+                current_date = commit.timestamp.date()
+                commits.append(commit)
+            elif commit.timestamp.date() != current_date:
+                # Add commits to the dict under the date which they occurred
+                daily_commits[current_date.isoformat()] = current_commits
+                commits = []
+
+                # Update the date and add the current commit to the now empty list
+                current_date = commit.timestamp.date()
+                commits.append(commit)
+            elif commit.timestamp.date() == current_date:
+                commits.append(commit)
+
+        return daily_commits
+
+    def commitsByDay_OLD(self):
         days = []
         current_date = 0
         current_day = {}
@@ -267,8 +290,6 @@ class GitCommit:
                 )
         return file_list
 
-    timestamp = property(operator.attrgetter("_timestamp"))
-
     files = property(operator.attrgetter("_files"))
 
     @files.setter
@@ -282,6 +303,8 @@ class GitCommit:
     def timestamp(self, timestamp):
         if isinstance(timestamp, date):
             self._timestamp = timestamp
+        else:
+            print("timestamp is not a timestamp")
 
     time_delta = property(operator.attrgetter("_time_delta"))
 
@@ -291,6 +314,7 @@ class GitCommit:
         if isinstance(time_delta, timedelta):
             self._time_delta = time_delta
         else:
+            print("time_delta is not a time delta")
             self._time_delta = timedelta(0)
 
     additions = property(operator.attrgetter("_additions"))
