a710a699608d5fd6330189294b6c087fffd710e3
jarett-lee <jarett.lee.pi@gmail.com>
2018-12-07 02:33:42 -0500

    Customize diff script for shell project

--- server/src/main/bash/calculateDiffScore.sh
+++ server/src/main/bash/calculateDiffScore.sh
@@ -6,8 +6,20 @@
 
 # DIRECTORY="$(pwd)"
 # cs252 last commit hash 6f284e205ac396c895ca42ca200105fb86822b7b
-user1=$(git --git-dir="$1/.git" diff --ignore-all-space "$3" HEAD | grep -e '^[+]' | grep -ve '^[+]\{3\}')
-user2=$(git --git-dir="$2/.git" diff --ignore-all-space "$3" HEAD | grep -e '^[+]' | grep -ve '^[+]\{3\}')
-out=$(grep -xFf <(echo "$user1") <(echo "$user2") | grep -vE '^\+\s*[{}]?$' | grep -vE '^\+\s*(return|#include).+$' | wc -l)
+
+# only works for cs252 shell project
+
+declare -a arr=("shell.y" "shell.l" "shell.cc")
+out=0
+
+# loop through array
+for i in "${arr[@]}"
+do
+    # specific file diff
+    user1=$(git --git-dir="$1/.git" diff --ignore-all-space "$3:$i" "HEAD:$i" | grep -e '^[+]' | grep -ve '^[+]\{3\}')
+    user2=$(git --git-dir="$2/.git" diff --ignore-all-space "$3:$i" "HEAD:$i" | grep -e '^[+]' | grep -ve '^[+]\{3\}')
+    temp=$(grep -xFf <(echo "$user1") <(echo "$user2") | grep -vE '^\+\s*[{}]?$' | grep -vE '^\+\s*(return|#include|else).*$' | grep -vE '^\+\s*(/\*|\*/|;)$' | wc -l)
+    out=$((out + temp))
+done
 
 echo "$out"
