dc1910e6a9375fb820ff331d3cef1801a5d06375
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2019-01-22 21:51:02 -0500

    Add script to generate diffs for all commits after given timestamp

---
+++ server/src/main/bash/generateDiffsAfterDate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Argument 1 is the path to the student project to get diffs from
+# Argument 2 is the timestamp that diffs are generated after
+
+DIRECTORY="$(pwd)"
+cd "$1"
+git log --perl-regexp --author='^((?!CS252).*)$' --pretty=format:"%H" --reverse --after="$2" | while read p; do
+    git show -U0 "$p" --date=short --pretty=format:"@DIFF,%H,%ad" --ignore-all-space | awk '{gsub(" ", "", $0);gsub("\t", "", $0); print}'
+done
+cd "${DIRECTORY}"
