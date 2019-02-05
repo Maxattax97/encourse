19f42cb5850d8e38f4d02d208c1042ed3601fd11
Killian-LeClainche <kleclain@purdue.edu>
2019-02-04 22:32:57 -0500

    Fix Query

--- server/src/main/java/edu/purdue/cs/encourse/database/ProjectRepository.java
+++ server/src/main/java/edu/purdue/cs/encourse/database/ProjectRepository.java
@@ -10,10 +10,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-    Project findByProjectID(@NonNull String projectID);
-    List<Project> findBySemester(@NonNull String semester);
-    List<Project> findBySemesterAndCourseID(@NonNull String semester, @NonNull String courseID);
-    boolean existsByProjectID(@NonNull String projectID);
     
     @Query("select p from Project p where p.dueDate >= p.analyzeDateTime")
     List<Project> findAllProjectsByAnalyzeDate();
