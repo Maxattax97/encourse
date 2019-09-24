102d5665e7abe5f09f50f3c472f068429f2405c5
Jordan Buckmaster <buckmast@purdue.edu>
2019-09-24 19:35:17 -0400

    Search for user email in database

--- server/src/main/java/edu/purdue/cs/encourse/controller/SigninController.java
+++ server/src/main/java/edu/purdue/cs/encourse/controller/SigninController.java
@@ -1,9 +1,9 @@
 package edu.purdue.cs.encourse.controller;
-import edu.purdue.cs.encourse.database.UserRepository;
+import edu.purdue.cs.encourse.database.AccountRepository;
 import edu.purdue.cs.encourse.auth.Authenticator;
 import edu.purdue.cs.encourse.auth.Sender;
 import edu.purdue.cs.encourse.auth.TokenStore;
-import edu.purdue.cs.encourse.domain.User;
+import edu.purdue.cs.encourse.domain.Account;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +19,17 @@ public class SigninController {
   
   private final TokenStore tokenStore;
 
-  private final UserRepository userRepository;
+  private final AccountRepository accountRepository;
   
   private final Sender sender;
   
   private final Authenticator authenticator;
   
-  public SigninController (TokenStore aTokenStore, Sender aSender, Authenticator aAuthenticator, UserRepository aUserRepository){
+  public SigninController (TokenStore aTokenStore, Sender aSender, Authenticator aAuthenticator, AccountRepository aAccountRepository){
     tokenStore = aTokenStore;
     sender = aSender;
     authenticator = aAuthenticator;
-    userRepository = aUserRepository;
+    accountRepository = aAccountRepository;
   }
 
   @GetMapping("/signin")
@@ -41,12 +41,11 @@ public class SigninController {
   public String signin (@RequestParam("username") String aUsername) {
     
     // verify that the user is in the database.
-    List<User> users= userRepository.findAll();
-    System.out.println("===========================================\n===========================================\n===========================================\n===========================================\n===========================================\n");
-    System.out.println(users);
+    String email = verifyUser(aUsername)
+
     // send sign-in email
     String token = tokenStore.create(aUsername);
-    sender.send(""/*aEmail*/, token);
+    sender.send(email, token);
     
     return "login_link_sent";
   }
@@ -61,5 +60,16 @@ public class SigninController {
       return "invalid_login_link";
     }
   }
-  	
+
+  private String verifyUser(String username) {
+    List<Account> accounts = accountRepository.findAll();
+    for(Account account : accounts) {
+      System.out.println("Account Username: " + account.getUsername())
+      if(account.getUsername().equals(username)) {
+        return account.getEduEmail();
+      }
+    }
+
+    return null;
+  }
 }
