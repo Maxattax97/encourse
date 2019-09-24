505210385d66a1966c0ecff896649511dae11486
Jordan Buckmaster <buckmast@purdue.edu>
2019-09-24 19:10:05 -0400

    Add debug statements

--- server/src/main/java/edu/purdue/cs/encourse/auth/EmailSender.java
+++ server/src/main/java/edu/purdue/cs/encourse/auth/EmailSender.java
@@ -28,6 +28,9 @@ public class EmailSender implements Sender {
     public void send (String aUserId, String aToken) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
 
+        System.out.println("from: " + from);
+        System.out.println("userId: " + aUserId);
+        System.out.println("Token: " + aToken);
         mailMessage.setFrom(from);
         mailMessage.setTo(aUserId);
         mailMessage.setSubject("Your signin link");
