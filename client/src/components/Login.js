5f6c0f75415804355b6c7575553e145af79a09c3
Jordan Buckmaster <buckmast@purdue.edu>
2019-09-24 21:14:29 -0400

    Remove password field

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -33,8 +33,6 @@ class Login extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <h3>Username</h3>
                         <input type="text" name="username" />
-                        <h3>Password</h3>
-                        <input type="password" name="password" />
                         <input type="submit" value="Enter Credentials" />
                     </form>
                 </div>
